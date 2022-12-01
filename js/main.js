61f8f195a53c346780384cf2f622148b4d3ceb58
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-12-01 11:03:28 -0800

    Added documentation, refactored some parameter names Changed get all breeds to 10 results

--- js/main.js
+++ js/main.js
@@ -18,26 +18,27 @@ var BreedImage = (function () {
     }
     return BreedImage;
 }());
-function getByID(id) {
-    return document.getElementById(id);
+function getByID(elementID) {
+    return document.getElementById(elementID);
 }
-function getInputByID(id) {
-    return getByID(id);
+function getInputByID(inputID) {
+    return getByID(inputID);
 }
-function getInputValueByID(id) {
-    return getByID(id).value;
+function getInputValueByID(inputID) {
+    return getByID(inputID).value;
 }
 window.onload = function () {
     var getAllBreedsButton = getByID("get-all-breeds");
     getAllBreedsButton.onclick = getAllBreeds;
 };
 function getAllBreeds() {
-    fetch('https://api.thedogapi.com/v1/breeds')
+    fetch('https://api.thedogapi.com/v1/breeds?limit=10&page=0')
         .then(function (response) { return response.json(); })
         .then(function (data) {
+        console.log(data);
         for (var currIndex = 0; currIndex < data.length; currIndex++) {
             var currBreed = data[currIndex];
-            var breedDisplay = "\n            <div class=\"col\">\n                <div class=\"card\">\n                    <img src=\"".concat(currBreed.image.url, "\" class=\"card-img-top\" alt=\"Image of ").concat(currBreed.name, "\">\n                    <div class=\"card-body\">\n                        <h5 class=\"card-title\">").concat(currBreed.name, "</h5>\n                        <p class=\"card-text\">Other info coming soon...</p>\n                    </div>\n                </div>\n            </div>");
+            var breedDisplay = "\n            <div class=\"col\">\n                <div class=\"card\">\n                    <img src=\"".concat(currBreed.image.url, "\" class=\"card-img-top\" alt=\"Image of ").concat(currBreed.name, "\">\n                    <div class=\"card-body\">\n                        <h5 class=\"card-title\">").concat(currBreed.name, "</h5>\n                        <p class=\"card-text\">Often bred for: ").concat(currBreed.bred_for, "</p>\n                    </div>\n                </div>\n            </div>");
             var breedDisplayDiv = document.getElementById("breed-display");
             breedDisplayDiv.innerHTML += breedDisplay;
         }
